1ae281c7cd02b0e44dff5a833eb52587ba3bbdca
Zhongjiting <153809935+Zhongjiting@users.noreply.github.com>
2025-01-06 12:12:20 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -236,8 +236,8 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="Cat AI"
+        subTitle="Cat 自建AI站"
         logo={<ChatGptIcon />}
         shouldNarrow={shouldNarrow}
       >
